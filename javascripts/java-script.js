8a11500fc5be218fa8c3aede6e3d4709930d037a
nonitut <nona.blogg@gmail.com>
2024-07-25 16:12:54 +0300

    Sammary at this day
    a lot of  news

--- javascripts/java-script.js
+++ javascripts/java-script.js
@@ -1,5 +1,27 @@
 document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById('flavorSelect').addEventListener('change', function() {
+        let select = this;
+        let selectedOption = select.options[select.selectedIndex];
+        select.style.backgroundColor = window.getComputedStyle(selectedOption).backgroundColor;
+        select.style.color = '#ffffff'; 
+    });
+
+    const popupButton = document.getElementById('popup');
+    const modal = document.getElementById('order-modal');
+    const closeModal = document.getElementById('close-modal');
+
+    popupButton.addEventListener('click', () => {
+        modal.style.display = 'flex';
+    });
 
 
-    
+    closeModal.addEventListener('click', () => {
+        modal.style.display = 'none';
+    });
+
+    window.addEventListener('click', (event) => {
+        if (event.target === modal) {
+            modal.style.display = 'none';
+        };
+    });
 });
